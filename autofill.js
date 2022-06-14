fce04df6b20a564bd0280ba9ab903083e2e99b28
Kfir Eichenblat <67231179+keichenblat@users.noreply.github.com>
2022-06-14 14:58:26 +0300

    Update autofill.js

--- autofill.js
+++ autofill.js
@@ -1,9 +1,6 @@
-/* 
-  Filling your monthly hour report again? Dreading the moment? Perhaps this can help!
-  Open your console, choose the frame that contains the time table, paste the attached code, hit ENTER and presto!
-*/
 
-if (!$('div#scrolledGridView tr:nth-last-child(1) > td:nth-child(3)')) {
+const requiredElement = $('div#scrolledGridView tr:nth-last-child(1) > td:nth-child(3)');
+if (!requiredElement || !requiredElement.length) {
   console.log('==== Auto-Fill error ====');
   console.log('Cannot fill hours: You must choose the correct frame in the selector under the "Console" tab (it is called "main_frame")');
   console.log('=========================');
@@ -15,13 +12,14 @@ if (!$('div#scrolledGridView tr:nth-last-child(1) > td:nth-child(3)')) {
      workTypeString = prompt("You must type either 'home' or 'office':");
   }
 
+  const WORK_FROM_HOME = 8;
+  const WORK_FROM_OFFICE = 5;
   const WORK_DAYS = [0, 1, 2, 3, 4];    // use 1, 2, 3, 4, 5 outside of Israel
   const DAY_START = "9:00";
   const DAY_END = "18:00";
   const WFH_HOURS = 8.4;                // Number of WFH hours
-  const WORK_TYPE = workTypeString.toLowerCase() === 'home' ? 8 : 5;                  // 5 - Work from office | 8 - Work from Home
-
-  // TODO: Try to add some interaction with the user to allow them to choose "work type" and maybe even fill in days off
+  
+  const WORK_TYPE = workTypeString.toLowerCase() === 'home' ? WORK_FROM_HOME : WORK_FROM_OFFICE;
 
   $('div#scrolledGridView tr:nth-last-child(1) > td:nth-child(3)')[0].click();
   const EXISTING_ENTRIES = $('[id^=gdvTS_rw_]').toArray().filter((e)=> /_TPDATE$/.test(e.id)).map((e)=> (e.title.match(/(?<month>\d+)\/(?<day>\d+)\/(?<year>\d+)$/) || {}).groups).filter(Boolean).map(({ month, day, year })=> new Date(Number(year), Number(month) - 1, Number(day)).getTime());
